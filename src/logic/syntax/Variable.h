118fbd180b3bac391f230847b96a0d5cb6fd5828
Jun Li <junli19860919@gmail.com>
2020-06-09 22:25:39 -0700

    Update Variable.h

--- src/logic/syntax/Variable.h
+++ src/logic/syntax/Variable.h
@@ -15,6 +15,9 @@ public:
     ~Variable() {};
 
     void operator=(const Variable& c) {name_ = c.name_; id_ = c.id_;};
+    
+    std::string getName() const {return name_;};
+    std::size_t getId() const {return id_;};
 
     friend std::size_t hash_value(const Variable& v);
 protected:
@@ -26,27 +29,46 @@ private:
     void serialize(Archive& ar, const unsigned int version);
 
     virtual Term* doClone() const;
-    virtual std::string doName() const {return name_;};
+    virtual std::string doName() const {return this->name_;};
     virtual bool doEquals(const Term& t) const;
     virtual std::size_t doHash() const;
     std::string name_;
     std::size_t id_;
-    bool operator==(const Variable& l, const Variable& r);
-    bool operator!=(const Variable& l, const Variable& r);
+    friend bool operator==(const Variable& l, const Variable& r);
+    friend bool operator!=(const Variable& l, const Variable& r);
+    friend bool operator< (const Variable& l, const Variable& r);
+    friend bool operator> (const Variable& l, const Variable& r);
 };
-
+bool operator==(const Variable& lhs, const Variable& rhs);
+bool operator!=(const Variable& lhs, const Variable& rhs);
+bool operator< (const Variable& lhs, const Variable& rhs);
+bool operator> (const Variable& lhs, const Variable& rhs);
 
 inline std::size_t Variable::doHash() const {return hash_value(*this);}
 
 // IMPLEMENTATION
 inline std::size_t hash_value(const Variable& v) {
+    std::size_t seed=0;
     boost::hash_combine(seed, v.name_);
     boost::hash_combine(seed, v.id_);
     return seed;
 }
 
-inline bool operator==(const Variable& lhs, const Variable& rhs) {return (lhs.name_ == rhs.name_ && lhs.id_ == rhs.id_);}
+inline bool operator==(const Variable& lhs, const Variable& rhs) {return (lhs.getName().compare(rhs.getName())==0 && lhs.getId() == rhs.getId());}
 inline bool operator!=(const Variable& lhs, const Variable& rhs) {return !operator==(lhs, rhs);}
+inline bool operator< (const Variable& lhs, const Variable& rhs) {
+    if (lhs.getName() < rhs.getName()){
+        return true;
+    }        
+    if (lhs.getName() > rhs.getName()){
+        return false;
+    }        
+    if (lhs.getId() < rhs.getId()){
+        return true;
+    }        
+    return false;
+}
+inline bool operator> (const Variable& lhs, const Variable& rhs) {return !operator< (lhs, rhs);}
 
 template <class Archive>
 void Variable::serialize(Archive& ar, const unsigned int version) {
