ffb10348288f3188fcafb9367d33b7ae2db368d2
Jun Li <junli19860919@gmail.com>
2020-06-09 18:39:14 -0700

    Update Variable.h

--- src/logic/syntax/Variable.h
+++ src/logic/syntax/Variable.h
@@ -9,11 +9,12 @@
 
 class Variable : public Term {
 public:
-    Variable(std::string name="_Unknown");
-    Variable(const Variable& c) : name_(c.name_) {};
+    Variable(std::string name="_Unknown"); 
+    Variable(std::string name, std::size_t id) : name_(name), id_(id) {};
+    Variable(const Variable& c) : name_(c.name_), id_(c.id_) {}; 
     ~Variable() {};
 
-    void operator=(const Variable& c) {name_ = c.name_;};
+    void operator=(const Variable& c) {name_ = c.name_; id_ = c.id_;};
 
     friend std::size_t hash_value(const Variable& v);
 protected:
@@ -29,6 +30,7 @@ private:
     virtual bool doEquals(const Term& t) const;
     virtual std::size_t doHash() const;
     std::string name_;
+    std::size_t id_;
     bool operator==(const Variable& l, const Variable& r);
     bool operator!=(const Variable& l, const Variable& r);
 };
@@ -38,8 +40,9 @@ inline std::size_t Variable::doHash() const {return hash_value(*this);}
 
 // IMPLEMENTATION
 inline std::size_t hash_value(const Variable& v) {
-    boost::hash<std::string> hasher;
-    return hasher(v.name_);
+    boost::hash_combine(seed, v.name_);
+    boost::hash_combine(seed, v.id_);
+    return seed;
 }
 
 inline bool operator==(const Variable& lhs, const Variable& rhs) {return (lhs.name_ == rhs.name_ && lhs.id_ == rhs.id_);}
@@ -53,6 +56,7 @@ void Variable::serialize(Archive& ar, const unsigned int version) {
             static_cast<Term*>(NULL)
             );
     ar & name_;
+    ar & id_;
 }
 
 #endif
